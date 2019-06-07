b0f584fc8f36832d31993bf83cf475dce3d467e2
yusuke <yusuke@newsdict.jp>
2019-06-07 19:28:59 +0900

    Prepare env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+redis = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
+
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}", namespace: ENV['SIDEKIQ_NAMESPACE'] }
+  config.redis = { url: "redis://#{redis['host']}", namespace: ENV['SIDEKIQ_NAMESPACE'] }
 end
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}", namespace: ENV['SIDEKIQ_NAMESPACE'] }
+  config.redis = { url: "redis://#{redis['host']}", namespace: ENV['SIDEKIQ_NAMESPACE'] }
 end
 Sidekiq::Web.app_url = ENV['ADMIN_URL']
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Sidekiq::Web => ENV['SIDEKIQ_WEB_URL'], constraints: SuperAdminConstraint.new, as: 'sidekiq_web'
-  mount RailsAdmin::Engine => ENV['ADMIN_URL'], as: 'rails_admin'
+  mount Sidekiq::Web => "/super_admin/sidekiq", constraints: SuperAdminConstraint.new, as: 'sidekiq_web'
+  mount RailsAdmin::Engine => "/admin", as: 'rails_admin'
   devise_for 'user', :controllers => {
     :registrations => 'admins/registrations'
   }
