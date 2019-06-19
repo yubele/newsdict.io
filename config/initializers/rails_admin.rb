ddbeb5c629c23b2b258635cbf3e15a74b26e4711
yusuke <yusuke@newsdict.jp>
2019-06-19 15:34:37 +0900

    Switch the puma binding port to socket.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,20 +4,19 @@ RailsAdmin.config do |config|
   config.authenticate_with do
     warden.authenticate! scope: :user
   end
-  config.current_user_method(&:current_user)
   config.authorize_with :cancancan, AdminAbility
   config.current_user_method { current_user }
   # default actions
-  #config.actions do
-  #  dashboard
-  #  index
-  #  new do
-  #    except  ['User']
-  #  end
-  #  bulk_delete
-  #  show
-  #  edit
-  #  delete
-  #  show_in_app
-  #end
+  config.actions do
+    dashboard
+    index
+    new do
+      except  ['User']
+    end
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+  end
 end
--- src/provisioning/startup.sh
+++ src/provisioning/startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source /etc/profile.d/rvm.sh
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
-RAILS_LOG_TO_STDOUT=true RAILS_ENV=production bin/rails server -b 0.0.0.0
+yarn install --check-files
+RAILS_ENV=production bin/rails assets:precompile
+RAILS_ENV=production bin/rails webpacker:compile
+RAILS_LOG_TO_STDOUT=true RAILS_ENV=production bin/rails server
