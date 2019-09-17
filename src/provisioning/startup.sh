2ca5aa08e5a2e663bce107d3114ef73ed3dc8e85
Yusuke <yusuke@newsdict.jp>
2019-09-17 14:10:50 +0000

    Runnable on production

--- src/provisioning/startup.sh
+++ src/provisioning/startup.sh
@@ -2,10 +2,10 @@
 . /etc/profile.d/rvm.sh
 cd /var/www/docker
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
-NODE_ENV=production yarn install --check-files
 RAILS_ENV=production bundle install --path .bundle --deployment --without development test --quiet
+NODE_ENV=production yarn install --check-files
 RAILS_ENV=production bundle exec bin/rails log:clear
-RAILS_ENV=production bundle exec bin/rails credentials:edit
+EDITOR="mate --wait" bundle exec bin/rails credentials:edit
 RAILS_ENV=production bundle exec bin/rails webpacker:compile
 RAILS_ENV=production bundle exec bin/rails assets:precompile
 RAILS_LOG_TO_STDOUT=true RAILS_ENV=production bundle exec bin/rails server
