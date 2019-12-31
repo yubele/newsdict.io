a44f5837d64f9a83e6783a478db41791b856aeba
yusuke <yusuke@newsdict.jp>
2019-12-31 03:09:49 +0000

    Updated the binstubs on startup

--- src/provisioning/startup/bootstrap.sh
+++ src/provisioning/startup/bootstrap.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
+# Initialize enviroment
 if [ ! -z $1 ];then
-  export RAILS_ENV=$1
-  export NODE_ENV=$1
+  export APP_TYPE=$1
+else
+  exit 1
 fi
+if [ ! -z $2 ];then
+  export RAILS_ENV=$2
+  export NODE_ENV=$2
+fi
+# Initialize nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Initialize rvm
 . /etc/profile.d/rvm.sh
+# Install the gems of bundler
 if [ "$RAILS_ENV" = "production" ];then
-  bundle install --path .bundle --deployment --without development test --quiet
-else
-  bundle install --path .bundle
+  bundle install --deployment --without development test --quiet
+elif [ "$APP_TYPE" = "web" ];then
+  bundle install
+fi
+# Recreate bins. In development, it only run on web server
+if [ "$RAILS_ENV" = "production" ] || [ "$APP_TYPE" = "web" ];then
+  rm -rf /var/www/docker/bin
+  bundle exec rake app:update:bin
+  bundle exec rails webpacker:binstubs
+  bundle exec spring binstub --all
+fi
+# Start webpack-dev-server
+if [ "$RAILS_ENV" != "production" ];then
   bin/webpack-dev-server &
 fi
