d8dece3705b7c6c25a0d1d105756c18a6606d520
Yusuke Abe <yusuke@newsdict.jp>
2020-05-09 05:29:05 +0000

    Refactoring the view of admin

--- lib/rails_admin/user.rb
+++ lib/rails_admin/user.rb
@@ -2,12 +2,12 @@ RailsAdmin.config do |config|
   config.model User do
     list do
       filters [:username, :email]
-      field :username do
-        visible true
-      end
-      field :email do
-        filterable true
-      end
+      field :username
+      field :email
+      field :sign_in_count
+      field :is_manual_locked
+      field :last_sign_in_at
+      field :last_sign_in_ip
     end
     edit do
       field :username do
@@ -16,6 +16,26 @@ RailsAdmin.config do |config|
       field :is_manual_locked do
         visible true
       end
+    end
+    show do
+      field :username
+      field :email
+      field :encrypted_password
+      field :reset_password_token
+      field :reset_password_sent_at
+      field :remember_created_at
+      field :sign_in_count
+      field :current_sign_in_at
+      field :last_sign_in_at
+      field :current_sign_in_ip
+      field :last_sign_in_ip
+      field :confirmation_token
+      field :confirmed_at
+      field :confirmation_sent_at
+      field :unconfirmed_email
+      field :failed_attempts
+      field :unlock_token
+      field :locked_at
     end
   end
 end
