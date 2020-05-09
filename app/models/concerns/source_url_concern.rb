7a9299e0b99ed01de22fc401d2ae1d5c969abada
Yusuke Abe <yusuke@newsdict.jp>
2020-05-09 04:50:58 +0000

    Fixed the extraction method of url.

--- app/models/concerns/source_url_concern.rb
+++ app/models/concerns/source_url_concern.rb
@@ -3,9 +3,27 @@ module SourceUrlConcern
 
   # Get external urls
   def urls
-    ::Nokogiri::HTML(Mechanize.new.get(source_url).body).xpath('//a').map {|a| a.attribute('href').value }.uniq
+    uri = URI.parse(source_url)
+    if [80, 443].include?(uri.port)
+      fqdn = "#{uri.scheme}://#{uri.host}"
+    else
+      fqdn = "#{uri.scheme}://#{uri.host}:#{uri.port}"
+    end
+    hrefs = Array.new
+    ::Nokogiri::HTML(Mechanize.new.get(source_url).body)
+    .xpath('//a').map {|a| a.attribute('href').value if a.attribute('href') }
+    .reject(&:blank?)
+    .uniq.each do |href|
+      if href.match(Regexp.new("^/"))
+        hrefs << "#{fqdn}#{href}"
+      elsif href.match(Regexp.new("^\."))
+        hrefs << "#{fqdn}/#{href}"
+      else
+        hrefs << "#{fqdn}#{uri.path}/#{href}"
+      end
+    end
+    hrefs
   end
-  
   class_methods do
   end
 end
