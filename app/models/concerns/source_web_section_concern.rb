03c246afaf72ec143aaaba1d13fec43ca0bef458
Yusuke Abe <yusuke@newsdict.jp>
2020-05-12 09:53:25 +0000

    Fixed urls

--- app/models/concerns/source_web_section_concern.rb
+++ app/models/concerns/source_web_section_concern.rb
@@ -3,8 +3,26 @@ module SourceWebSectionConcern
 
   # Get external urls
   def urls
+    uri = URI.parse(source_url)
+    if [80, 443].include?(uri.port)
+      fqdn = "#{uri.scheme}://#{uri.host}"
+    else
+      fqdn = "#{uri.scheme}://#{uri.host}:#{uri.port}"
+    end
+    hrefs = Array.new
     agent = Mechanize.new { |_agent| _agent.user_agent = WebStat::Configure.get["user_agent"] }
-    ::Nokogiri::HTML(agent.get(source_url).body).xpath("#{xpath}//a/@href").map {|a| a.value unless a.value.blank? }.uniq
+    ::Nokogiri::HTML(agent.get(source_url).body)
+    .xpath("#{xpath}//a/@href").map {|a| a.value unless a.value.blank? }
+    .uniq.each do |href|
+      if href.match(Regexp.new("^/"))
+        hrefs << "#{fqdn}#{href}"
+      elsif href.match(Regexp.new("^\."))
+        hrefs << "#{fqdn}/#{href}"
+      else
+        hrefs << "#{fqdn}#{uri.path}/#{href}"
+      end
+    end
+    hrefs
   end
   class_methods do
   end
