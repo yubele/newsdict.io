9d5599677985e74d960ccae316d1a2486510d59b
Ubuntu <ubuntu@ip-172-31-43-43.us-west-2.compute.internal>
2020-05-16 06:01:33 +0000

    Limited number of registered users

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,4 +1,12 @@
 class Admin::RegistrationsController < Devise::RegistrationsController
+  # Limited number of registered users
+  def new
+    if ENV['MAX_MEMBER_COUNT'].to_i < User.all.count
+      redirect_to '/not_found'
+    else
+      super
+    end
+  end
   # Redirect to edit on rails_admin
   def edit
     redirect_to rails_admin.edit_path(model_name: :user, id: resource.id)
