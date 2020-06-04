2cdfa314b107282f1695787c8e20266b525c4828
Yusuke Abe <yusuke@newsdict.jp>
2020-06-04 13:00:16 +0000

    Fixed to wrong parameter.

--- app/models/sources/web_site.rb
+++ app/models/sources/web_site.rb
@@ -21,11 +21,11 @@ module Sources
       ::Nokogiri::HTML(WebDriverHelper.get_source(source_url))
       .xpath("#{xpath}//a/@href").map {|a| a.value unless a.value.blank? }
       .uniq.each do |href|
-        if href.match(Regexp.new("^/"))
+        if href.match(::Regexp.new("^/"))
           hrefs << "#{fqdn}#{href}"
-        elsif href.match(Regexp.new("^\."))
+        elsif href.match(::Regexp.new("^\."))
           hrefs << "#{fqdn}/#{href}"
-        elsif href.match(Regexp.new("^http"))
+        elsif href.match(::Regexp.new("^http"))
           hrefs << href
         else
           hrefs << "#{fqdn}#{uri.path}/#{href}"
