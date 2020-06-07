59383d1e0aad5a6e334f7de5d6ae8ce63ce4faf3
Yusuke Abe <yusuke@newsdict.jp>
2020-06-07 11:16:07 +0000

    Support asset for each theme

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,11 @@
+/**
+ * Custom assets for rails_admin
+ */
 //= link rails_admin/custom/theming.css
 //= require rails_admin/custom/ui.js
+
+/**
+ * Custom assets for theme.
+ */
 //= link custom.css
-//= require custom.js
+//= link custom.js
