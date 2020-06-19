7f86961020774c44a9d112f492855f308c4ce8f4
Yusuke Abe <yusuke@newsdict.jp>
2020-06-19 07:43:57 +0000

    Fixed worker errors.

--- app/helpers/web_driver_helper.rb
+++ app/helpers/web_driver_helper.rb
@@ -8,9 +8,7 @@ module WebDriverHelper
     options = Selenium::WebDriver::Chrome::Options.new(args: [
       'headless',
       'no-sandbox',
-      'disable-gpu',
-      'start-maximized',
-      'window-size=1920,1080'
+      'disable-gpu'
       ])
     driver = Selenium::WebDriver.for(:chrome, options: options)
     driver.manage.timeouts.implicit_wait = 10
