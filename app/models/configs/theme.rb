ff63acc0e53b6e660d2a84d90d3a9ebc1c6b434f
Yusuke Abe <yusuke@newsdict.jp>
2020-07-18 15:48:53 +0000

    Bugfixed: Considered already activated themes. / Added behavior when disabling everything.

--- app/models/configs/theme.rb
+++ app/models/configs/theme.rb
@@ -1,23 +1,32 @@
 class Configs::Theme < Config
+  DEFAULT_THEME_NAME = 'default'
   before_save do
     if is_active == true
       self.class.where(is_active: true).update(is_active: false)
     end
   end
+  after_save do
+    if is_active == false && Configs::Theme.activated_theme.nil?
+      self.class.where(key: Configs::Theme::DEFAULT_THEME_NAME).update(is_active: true)
+    end
+  end
   field :description, type: String
   field :is_active, type: Boolean, default: false
   class << self
+    # Get already activated theme.
+    # @return [Configs::theme] activated theme.
+    def activated_theme
+      find_by(is_active: true)
+    end
     # Apply theme
     # @return [Configs::Theme] Applied theme.
     def apply
-      apply_theme = find_by(is_active: true)
-      ActionController::Base.prepend_view_path  "app/themes/#{theme.key}"
-      I18n.load_path += Dir[Rails.root.join('app', 'themes', theme.key, 'locales', '*.{rb,yml}')]
-      Rails.application.config.assets.paths << Rails.root.join('app', 'themes', theme.key, 'assets', 'javascripts')
-      Rails.application.config.assets.paths << Rails.root.join('app', 'themes', theme.key, 'assets', 'stylesheets')
-      apply_theme
+      ActionController::Base.prepend_view_path  "app/themes/#{activated_theme.key}"
+      I18n.load_path += Dir[Rails.root.join('app', 'themes', activated_theme.key, 'locales', '*.{rb,yml}')]
+      Rails.application.config.assets.paths << Rails.root.join('app', 'themes', activated_theme.key, 'assets', 'javascripts')
+      Rails.application.config.assets.paths << Rails.root.join('app', 'themes', activated_theme.key, 'assets', 'stylesheets')
+      activated_theme
     end
-    
     # Get the names of app/themes/{name}.
     # @return [Array] names
     def directory_names
--- config/initializers/theme.rb
+++ config/initializers/theme.rb
@@ -1,8 +1,11 @@
+# Get already activated theme.
+activated_theme = Configs::Theme.activated_theme
 # Flush the theme names.
 Configs::Theme.delete_all
 # Import the theme names.
 Configs::Theme.directory_names.each do |key|
-  if key == 'default'
+  if (activated_theme.nil? && key == Configs::Theme::DEFAULT_THEME_NAME) ||
+    (activated_theme && key == activated_theme.key)
     is_active = true
   else
     is_active = false
