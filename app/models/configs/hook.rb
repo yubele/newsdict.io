e24916ce2f6f33b0375fc8f2781eff31574ff148
Yusuke Abe <yusuke@newsdict.jp>
2020-08-09 23:16:46 +0000

    Renamed fields to make it easier to understand.

--- app/models/configs/hook.rb
+++ app/models/configs/hook.rb
@@ -4,14 +4,14 @@ module Configs
       :requested_removing,
       :receive_form
     ]
-    field :chatwork_id, type: BSON::ObjectId
-    field :slack_id, type: BSON::ObjectId
-    belongs_to :slack, class_name: "Configs::Tokens::Slack", optional: true
-    belongs_to :chatwork, class_name: "Configs::Tokens::Chatwork", optional: true
-    def chatwork_id_enum
+    field :notify_chatwork_id, type: BSON::ObjectId
+    field :notify_slack_id, type: BSON::ObjectId
+    belongs_to :notify_slack, class_name: "Configs::Tokens::Slack", optional: true
+    belongs_to :notify_chatwork, class_name: "Configs::Tokens::Chatwork", optional: true
+    def notify_chatwork_id_enum
       Configs::Tokens::Chatwork.all.map {|c| [c.key, c.id] }.to_h
     end
-    def slack_id_enum
+    def notify_slack_id_enum
       Configs::Tokens::Slack.all.map {|c| [c.key, c.id] }.to_h
     end
     class << self
@@ -22,11 +22,11 @@ module Configs
       def trigger(key, data)
         raise "not found hook" if Configs::Hook::KEYS.find_index(:requested_removing).nil?
         hook = self.find_by(key: key)
-        if hook.slack_id.present?
-          hook.slack.send_message(data)
+        if hook.notify_slack_id.present?
+          hook.notify_slack.send_message(data)
         end
-        if hook.chatwork_id.present?
-          hook.chatwork.send_message(data)
+        if hook.notify_chatwork_id.present?
+          hook.notify_chatwork.send_message(data)
         end
       end
     end
