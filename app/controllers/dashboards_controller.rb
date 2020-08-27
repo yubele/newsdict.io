cb2c4b45406a0bf380c6165f8937eb8fbb1379c0
Yusuke Abe <yusuke@newsdict.jp>
2020-08-27 01:04:03 +0000

    Refactor dashboards_controller.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,30 +1,14 @@
 class DashboardsController < ApplicationController
   include Api::ContentsControllerConcern
-  before_action :init_vars
-  def init_vars
-    @per_page = Newsdict::Application.config.paper_item_limit
-    @column_number = Newsdict::Application.config.paper_column_number
-  end
   # Dashboard of authorized user.
   def show
-    time = Time.strptime('20200501', '%Y%m%d')
-    @paper = Paper.new
-    @paper.user = User.find_by(email: ENV['ADMIN_USER_EMAIL'])
-    @contents = Contents::Web.contents.sortable(params[:sort]).gte(:created_at => time.midnight).lte(:created_at => time.end_of_day).page(params[:page])
-    @paper.title = I18n.t('paper.one_day.title', date: I18n.l(time, format: :only_date))
+    @contents = Contents::Web.contents.sortable(params[:sort]).page(params[:page])
     render :show
   end
   # Search content
   # @param [String] keyword
   def search(keyword)
-    time = Time.strptime('20200501', '%Y%m%d')
-    @paper = Paper.new
-    @paper.user = User.find_by(email: ENV['ADMIN_USER_EMAIL'])
-    @contents = Contents::Web.contents.
-      any_of(
-        Contents::Web.search_by_title(keyword),
-        Contents::Web.search_by_tag(keyword),
-        Contents::Web.search_by_category_name(keyword))
+    @contents = Contents::Web.contents.search_by_mixed(keyword).page(params[:page])
     @paper.title = I18n.t('paper.one_day.title', date: I18n.l(time, format: :only_date))
     render :show
   end
