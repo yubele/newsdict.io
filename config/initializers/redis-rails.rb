cc3505dad2f489c00dab8735345e02fb2a152125
Yusuke Abe <yusuke@newsdict.jp>
2020-09-12 23:19:37 +0000

    Supoort redis cahching.

---
+++ config/initializers/redis-rails.rb
@@ -0,0 +1,15 @@
+redis = YAML.load(ERB.new(File.read("#{Rails.root}/config/redis.yml")).result)[Rails.env]
+
+Newsdict::Application.config.cache_store = :redis_store, {
+  host: "redis://#{redis['host']}/0/cache",
+  port: 6379,
+  namespace: ENV['NAMESPACE']
+}, { expires_in: 90.minutes }
+
+Newsdict::Application.config.session_store :redis_store,
+  servers: ["redis://#{redis['host']}/0/session"],
+  expire_after: 90.minutes,
+  key: "_#{Rails.application.class.parent_name.downcase}_session",
+  threadsafe: true,
+  signed: true,
+  secure: true
