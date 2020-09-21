6a46086d01f9b75cecb6016739bf3c71683b2a84
Yusuke Abe <yusuke@newsdict.jp>
2020-09-21 18:51:08 +0000

    Refactor statup shells.

--- src/provisioning/startup/bootstrap/production.sh
+++ src/provisioning/startup/bootstrap/production.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Recreate bins. It only run on web server and production.
-rm -rf /var/www/docker/bin
 bundle exec rake app:update:bin
 bundle exec rails webpacker:binstubs
 bundle exec spring binstub --all
