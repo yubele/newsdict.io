7ad3866c88a9562dd16bf58c951bdcfffdc74f3b
Yusuke Abe <yusuke@newsdict.jp>
2020-09-21 16:35:22 +0000

    Fixed to work

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge');
 const fs = require('fs');
 
 module.exports = merge(environment.toWebpackConfig(), {
