16ac5c3faf8fe6d173b40f685115b967597fd9c7
Yusuke Abe <yusuke@newsdict.jp>
2020-09-22 00:59:56 +0000

    Update webpacker.

--- app/javascript/imports/fontawesome.js
+++ app/javascript/imports/fontawesome.js
@@ -1,5 +1,5 @@
-import '@fortawesome/fontawesome-free/scss/fontawesome';
-import '@fortawesome/fontawesome-free/scss/solid';
-import '@fortawesome/fontawesome-free/scss/regular';
-import '@fortawesome/fontawesome-free/scss/brands';
-import '@fortawesome/fontawesome-free/scss/v4-shims';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+import '@fortawesome/fontawesome-free/scss/v4-shims.scss';
