6fe1933fe86f5101799b22f99f94e191a8ec4eb3
Yusuke Abe <yusuke@newsdict.jp>
2020-09-22 00:59:18 +0000

    Fixed to work batch.

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,11 +2,13 @@ class Batch < ApplicationRecord
   include Mongoid::Document
   field :name, type: String
   include Mongoid::Timestamps
-  validates :name, presence: true, uniqueness: true, length: {minimum: 2, maximum: 255}
+  validates :name, presence: true, uniqueness: true
   DEFAULT_EXPIRE_ONETIME = 1.minutes.ago
   class << self
     def onetime(name)
-      if find_or_create_by(:name => name, :updated_at.gt => self::DEFAULT_EXPIRE_ONETIME).persisted?
+      if find_by(name: name, :updated_at.gt => self::DEFAULT_EXPIRE_ONETIME).nil? &&
+          # If this method called at initilizer, Auto timestamp not working.
+          find_or_create_by(name: name)
         yield
       end
     end
