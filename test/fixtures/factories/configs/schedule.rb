44922766d5fb6451e386f70bc1dfa2507d29d59d
Yusuke Abe <yusuke@newsdict.jp>
2020-10-24 08:18:29 +0000

    Fixed Configs::Schedule factory.

--- test/fixtures/factories/configs/schedule.rb
+++ test/fixtures/factories/configs/schedule.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory "Configs::Schedule" do
     key { Faker::Name.unique.name }
     description { Faker::Name.unique.name }
-    hour { rand(1..24) }
-    min { rand(1..59) }
+    hour { rand(0..24) }
+    min { rand(0..59) }
     wday { rand(0..6) }
   end
 end
