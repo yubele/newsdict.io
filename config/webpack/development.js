1151ed249631baa37b131cfea758f051e9456bac
Yusuke Abe <yusuke@newsdict.jp>
2020-10-31 12:58:25 +0000

    Refactor autoload article

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,6 +5,7 @@ const { merge } = require('webpack-merge');
 const fs = require('fs');
 
 module.exports = merge(environment.toWebpackConfig(), {
+    mode: 'development',
     devServer: {
         public: process.env.WEBPACK_DEV_SERVER_PUBLIC + ":3035",
         // refs. https://memo.willnet.in/entry/2017/07/30/234412
