630285a093fd1dee3f30e072869990f03c653b43
Yusuke Abe <yusuke@ybj.jp>
2020-12-12 14:34:49 +0900

    Fxied to work /admin.

--- config/initializers/theme.rb
+++ config/initializers/theme.rb
@@ -1,6 +1,4 @@
-Rails.configuration.to_prepare do
-  Batch.single_server(:theme) do
-    Configs::Theme.tidy
-  end
-  Configs::Theme.apply
+Batch.single_server(:theme) do
+  Configs::Theme.tidy
 end
+Configs::Theme.apply
