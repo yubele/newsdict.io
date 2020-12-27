e74d52b4c85f78e4a1c04558b5d0962308c5d762
Yusuke Abe <yusuke@ybj.jp>
2020-12-27 09:21:33 +0900

    Fix typo.

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -1,5 +1,5 @@
-class Api::V1::ThemesController < ApplicationController
-  include Api::ThemessControllerConcern
+class Api::V1::ContentsController < ApplicationController
+  include Api::ContentsControllerConcern
   # show
   #  /api/v1/contents.json?skip=[\d]+&limit=[\d]+&sort=(desc|asc)&category=xxx
   def show
