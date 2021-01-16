b8e0c578dbf1a8e55b6cc3aa8034235c198e4d77
Yusuke Abe <yusuke@ybj.jp>
2021-01-16 23:23:06 +0900

    Suppress zeitwerk's `Deprecate Warning`.

--- config/initializers/theme.rb
+++ config/initializers/theme.rb
@@ -1,8 +1,8 @@
-Batch.single_server(:theme) do
-  Configs::Theme.tidy
-end
-Configs::Theme.apply
-Rails.application.config.after_initialize do
+Rails.application.reloader.to_prepare do
+  Batch.single_server(:theme) do
+    Configs::Theme.tidy
+  end
+  Configs::Theme.apply
   Rails.application.config.assets.paths.delete(Rails.root.join('app', 'assets', 'javascripts').to_s)
   Rails.application.config.assets.paths.delete(Rails.root.join('app', 'assets', 'stylesheets').to_s)
   Rails.application.config.assets.paths.delete(Rails.root.join('app', 'assets', 'images').to_s)
