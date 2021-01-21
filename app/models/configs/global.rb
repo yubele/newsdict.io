9b18b7b8d3cb2f61d7f8c3f46d59b21dbd1cdba6
yusuke <yusuke@ybj.jp>
2021-01-21 10:33:55 +0900

    Fix_to_model_filename. (#167)
    * Fix `uninitialized constant NestedForm::BuilderMixin (NameError)`.
    
    * Targeted for actions when pushing to the main branch.
    
    * Fix to import the assets of webpacker on production.
    
    * Made test data static due to incorrect characters
    
    * Work to sidekiq-scheduler.
    
    * View all field for contents models
    
    * Fix to work api.
    
    * Fixe to the argument errors.
    
    * Prepared a toggle button to enable Google Translate.
    
    * Fix to model filename.
    
    * Fix to typo.
    
    * Fix to 'undefined method `value`'.

--- app/models/configs/global.rb
+++ app/models/configs/global.rb
@@ -3,6 +3,7 @@ class Configs::Global < Config
     system("bin/rails restart")
   end
   field :value, type: String
+  field :enabled, type: Boolean
   KEYS = [
     :recaptcha_v3_secret_key,
     :recaptcha_v3_site_key,
