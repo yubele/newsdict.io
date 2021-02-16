67bb8bac1f1e81aba837609632e1ce5398e6d061
Yusuke Abe <yusuke@ybj.jp>
2021-02-16 10:26:41 +0900

    Since it reloads when the page changes or when using the development tool, it does not reload until 1 hour has passed.

--- app/javascript/imports/autoload.js
+++ app/javascript/imports/autoload.js
@@ -1,8 +1,11 @@
 (function(){
+  const timeNotToReload = 60 * 60 * 60 * 1000
+  const startMillisecond = window.performance.now()
   window.addEventListener('DOMContentLoaded', function(){
     window.addEventListener('focus', function(){
-      const datetime = new Date()
-      window.location.reload()
+      if (startMillisecond >= timeNotToReload) {
+        window.location.reload()
+      }
     });
   })
 })()
