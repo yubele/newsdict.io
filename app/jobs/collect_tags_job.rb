bd0b3b017c8685257ab89241d751bba07bd6d65f
Yusuke Abe <yusuke@ybj.jp>
2021-02-25 06:00:33 +0900

    Clean the collect_tag

--- app/jobs/collect_tags_job.rb
+++ app/jobs/collect_tags_job.rb
@@ -5,5 +5,9 @@ class CollectTagsJob < ApplicationJob
         CollectTag.add(name)
       end
     end
+    # clean tags
+    CollectTag.all.each do |tag|
+      tag.delete if Content::search_by_tag(tag: tag.name).count == 0
+    end
   end
 end
